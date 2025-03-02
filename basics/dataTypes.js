a631e0ccca56a50171d4f9ed6e5fd479a1d38a78
Nikhilesh Tiwari <tnikhilesh0231@gmail.com>
2025-03-02 18:05:39 +0000

    dataTypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";//treat all JS code as newr version
+
+// alert("hello");//It generate a pop in a our browser screen and replecated in browser.
+
+console.log(3
+    +
+    4);//Code is correct but this is not good practice to write code like this because it breaks the code readability very high
+console.log("Nikhilesh");
+
+let name = "stringDataType"//string
+let age = 18//number
+let isLogin = false//boolean
+let state;//undefined
+let temp = null//null
+
+//typeof
+console.table([typeof(name),typeof(age),typeof(isLogin),typeof(state),typeof(temp)])
+// ┌─────────┬─────────────┐
+// │ (index) │ Values      │
+// ├─────────┼─────────────┤
+// │ 0       │ 'string'    │
+// │ 1       │ 'number'    │
+// │ 2       │ 'boolean'   │
+// │ 3       │ 'undefined' │
+// │ 4       │ 'object'    │
+// └─────────┴─────────────┘
+
--- basics/variables_and_constants.js
+++ basics/variables_and_constants.js
@@ -3,11 +3,13 @@ const accountId = 23643839
 console.log(accountId);
 let accountEmail = "nikhil@google.com"
 var accountPassword = "1234455"//It's recommended to not use var because it has no scope 
+let accountState;
 {
     var accountPassword = "ndkf932ewiwe"
 }
 console.log(accountPassword);//here ndkf932ewiwe
 //but this problem cannot be seen in let 
+/* Prefer not to use var because of issue in block scope and functional scope */
 
 {
     let accountEmail = "veeru@gmail.in"
@@ -22,7 +24,7 @@ accountEmail = "nikhilesh@gmail.in"
 accountPassword = "9875242"
 accountCity = "Gurgaon"
 
-console.table([accountId,accountEmail,accountPassword,accountCity])
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
 // ┌─────────┬──────────────────────┐
 // │ (index) │ Values               │
 // ├─────────┼──────────────────────┤
@@ -30,4 +32,5 @@ console.table([accountId,accountEmail,accountPassword,accountCity])
 // │ 1       │ 'nikhilesh@gmail.in' │
 // │ 2       │ '9875242'            │
 // │ 3       │ 'Gurgaon'            │
+// │ 4       │ undefined            │         
 // └─────────┴──────────────────────┘
