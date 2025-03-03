eb9bbd30778c2ba3606fbeac51a976c9e1fc4859
Nikhilesh Tiwari <tnikhilesh0231@gmail.com>
2025-03-03 08:29:49 +0000

    comparison

---
+++ basics/comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1)//true
+// console.log(2 < 1)//false
+// console.log( 2 >= 1)//true
+// console.log(2 <= 1)//false
+// console.log(2 == 1)//false
+// console.log(2 != 1)//true
+
+// console.log("2" > 1);//true
+// console.log("02" > 1);//true
+
+// console.log(null > 0);//false
+// console.log(null == 0);//false
+// console.log(null >= 0);//true
+
+
+// console.log(undefined == 0);//false
+// console.log(undefined > 0);//false
+// console.log(undefined < 0);//false
+
+// === Strict check it check for the value and the data type to
+
+console.log("1"== 1);//true //its because here by default coversion is done
+console.log("1"=== 1);//false //its does not allow anuy conversion 
