09f099ca407ab166e11b2ab32409ad9784a537f5
Nikhilesh Tiwari <tnikhilesh0231@gmail.com>
2025-03-03 09:00:16 +0000

    dataType sumup

--- basics/dataTypes.js
+++ basics/dataTypes.js
@@ -25,3 +25,47 @@ console.table([typeof(name),typeof(age),typeof(isLogin),typeof(state),typeof(tem
 // │ 4       │ 'object'    │
 // └─────────┴─────────────┘
 
+
+//Two main types are 
+// 1.Primitive data type /call by value //7 
+// 1.string
+// 2.number
+// 3.boolean
+// 4.null
+// 5.Undefined
+// 6.symbol
+// 7.BigInt
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)//false
+
+
+const bigNumber = 3892787328271122112n
+console.log(typeof bigNumber);//bigint
+
+
+
+// Java Script is dynamically typed language
+
+// 2.Non-rimitive data type/refrence Type
+// 1.Arrays
+// 2.Objects
+// 3.Functions
+
+const heroes = ["shaktiman","Spiderman","superman","doga"]
+console.log(typeof heroes);//Object
+
+
+const student = {
+    name:"Veeru",
+    age:23,
+}
+console.log(typeof student);//Object
+
+const myFunc = function() {
+    console.log("Hello")
+}
+myFunc()//Hello
+console.log(typeof myFunc)//Function
