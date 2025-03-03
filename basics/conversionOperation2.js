90539419ee0e5edb3b40da40f5d890949d9d8247
Nikhilesh Tiwari <tnikhilesh0231@gmail.com>
2025-03-03 08:10:03 +0000

    operatons

---
+++ basics/conversionOperation2.js
@@ -0,0 +1,36 @@
+// ***************************  Operations  ****************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);//-3
+
+console.log(2+2);//4
+console.log(3-2);//1
+console.log(2*2);//4
+console.log(2**3);//2*2*2 = 8
+console.log(4/2);//2
+console.log(6%4);// It returns remainder as 2
+
+let str1 = "Hello"
+let str2 = " Nikhilesh"
+let str3 = str1 +str2
+console.log(str3);//Hello Nikhilesh
+
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log("1"+"2");//12
+
+console.log("1"+2+2);//122
+console.log(2+2+"1");//41
+
+
+console.log(+true);//1
+console.log(+"");//0
+
+let num1, num2,num3
+num1 = num2 = num3 = 2 + 2
+console.log(num1,num2,num3);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);//101
