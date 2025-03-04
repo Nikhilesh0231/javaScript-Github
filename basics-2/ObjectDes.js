d605dc4cbb8dc0638aebfbc94fac717db990b921
Nikhilesh Tiwari <tnikhilesh0231@gmail.com>
2025-03-04 19:09:25 +0000

    object destructuring

---
+++ basics-2/ObjectDes.js
@@ -0,0 +1,18 @@
+const user = {
+    name:"Nikhilesh",
+    age:22
+}
+
+console.log(user.name);//Nikhilesh
+
+//object destructuring
+
+const { name } = user
+console.log(name);//Nikhilesh
+
+// also we can change key name while destructuring
+
+const { age:StudentAge } = user
+console.log(StudentAge);//22
+
+
