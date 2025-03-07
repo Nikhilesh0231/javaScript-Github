c1816d5789309ad9c90aace26a3325a63c647a06
Nikhilesh Tiwari <tnikhilesh0231@gmail.com>
2025-03-07 07:56:03 +0000

    while loop

---
+++ loops/while_dowhile.js
@@ -0,0 +1,24 @@
+// while
+// while (condition) {
+    
+// }
+let index = 0;
+while(index <= 10){
+    console.log(index);
+    index=index+2;
+}
+
+let myArray = [10,20,30,40,50];
+let i = 0;
+while(i<myArray.length){
+    console.log(myArray[i]);
+    i++;
+}
+
+
+// doWhile
+
+// do {
+    
+// } while (condition);
+
