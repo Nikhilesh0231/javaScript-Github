4776ea6454ee13ffdd90ae1b1c9eb9207d8229d8
Nikhilesh Tiwari <tnikhilesh0231@gmail.com>
2025-03-08 18:53:34 +0000

    map

---
+++ loops/map.js
@@ -0,0 +1,17 @@
+const myNums = [1,2,3,4,5,6,7,8,9,10];
+
+const newNums = myNums.map( (num) => num+10 )
+
+console.log(newNums);
+
+
+// [
+//     11, 12, 13, 14, 15,
+//     16, 17, 18, 19, 20
+// ]  
+
+
+const newNums1 = myNums.map( (num) => num * 10)
+                        .map( (num) => num + 1)
+                
+console.log(newNums1); 
