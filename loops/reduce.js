48ad64a670f28ae7e777f8e453813c529f8028f3
Nikhilesh Tiwari <tnikhilesh0231@gmail.com>
2025-03-09 17:03:06 +0000

    newReduceExample

--- loops/reduce.js
+++ loops/reduce.js
@@ -7,3 +7,11 @@ let myTotal = MyNums.reduce(
 
 
 console.log(myTotal);//6
+
+const newNum = [2,4,6,8,10,12,14,16,18,20];
+
+const myNewTotal = newNum.reduce(
+    (acc,val) => acc +val,initVal
+)
+
+console.log(myNewTotal)//110
